217e33cd0894d6eaa986c60b4d59c2798cc78ab7
Dedsec <95139860+Musadiq235@users.noreply.github.com>
2022-04-14 11:12:14 +0500

    Add files via upload

---
+++ Lets gc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//The Factorial of a number
+
+	int number;
+	cout << "Number: ";
+	cin >> number;
+
+	int factorial = 1;
+
+	for (int i = 1; i <= number; i++) {
+		factorial = factorial * i;
+	}
+	cout << number << "!=" << factorial;
+
+	system("pause>0");
+}
