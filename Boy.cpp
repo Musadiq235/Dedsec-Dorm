fd0ba323b42da00659d41e91b8c9f1d53e648b9c
Dedsec <95139860+Musadiq235@users.noreply.github.com>
2022-04-15 13:15:59 +0500

    Add files via upload

---
+++ Boy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void function();
+
+int main() {
+
+	cout << "I am Tony Stark " << endl;
+	function();
+
+	system("pause>0");	
+}
+
+void function() {
+	cout << "We are Venom " << endl;
+}
